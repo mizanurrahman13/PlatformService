c51ec9db861df995f33e7deb9ea57fcad3800526
mizanurrahman13 <mizan.441980@gmail.com>
2025-02-13 02:12:22 +0600

    Added CommandsService Project

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1977d02a006fe85dd531ed13716f49db09350c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3809dcc83a4ecc026aeb9cc06932223a2fac3cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
