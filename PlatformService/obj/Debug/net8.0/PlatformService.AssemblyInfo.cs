027ee86d82c96b522f0c46cf8fd6db4b62c5b13c
mizanurrahman13 <mizan.441980@gmail.com>
2025-02-14 22:34:01 +0600

    Added Dockefile for CommandsService Project

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51ec9db861df995f33e7deb9ea57fcad3800526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7783dce1429c9179e4cbf61567716b432d1eeaa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
